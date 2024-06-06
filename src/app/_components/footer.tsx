eb9bc250bf8f3548f6af85d1ac1cc754ffcd8812
Seth Daily [SSW] <130118701+sethdaily@users.noreply.github.com>
2024-06-06 00:59:02 -0600

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href={`https://github.com/sethdaily/dont-be-stupid`}
-              className="mx-3 font-bold hover:underline"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               GitHub
             </a>
