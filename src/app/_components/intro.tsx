c6ecbfbe4ecc1c3703251368d2a6b245d633acbe
Seth Daily [SSW] <130118701+sethdaily@users.noreply.github.com>
2024-06-06 20:46:58 -0600

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
-        how to not be an idiot in 2024
+        A weekly guide on how to be a better person.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A weekly blog written with {CMS_NAME}.
