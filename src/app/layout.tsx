59906cbbf8e73b55449da93b968572a6170bda8c
Seth Daily [SSW] <130118701+sethdaily@users.noreply.github.com>
2024-06-06 20:43:18 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `How not to be an idiot in 2024`,
+  description: `A weekly guide written with ${CMS_NAME}.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
