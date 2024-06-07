39f10e9e63babf51114dc23a628c57ca4d24333a
Seth Daily [SSW] <130118701+sethdaily@users.noreply.github.com>
2024-06-07 02:55:45 -0600

    Update alert.tsx

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -27,17 +27,6 @@ const Alert = ({ preview }: Props) => {
               </a>{" "}
               to exit preview mode.
             </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
           )}
         </div>
       </Container>
